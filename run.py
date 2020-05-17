737bebb59ac4b8a50bf39f73d40e1104b44a3f13
Ben <bhart@aut.ac.nz>
2020-05-17 15:52:01 +1200

    added git update to run.py

--- run.py
+++ run.py
@@ -1,9 +1,16 @@
 from talk_to_nano import Getnanoreadings
+from plots import Genplots
 import time
+import os
 
 reading = Getnanoreadings()
+plots = Genplots()
 
 while True:
 	reading.sql_insert()
 	print('done {}'.format(time.ctime()))
+	plots.gen_plots()
+	os.system('git add *')
+	os.system('git commit -m "update {}"'.format(time.ctime()))
+	os.system('git push')
 	time.sleep(300)
