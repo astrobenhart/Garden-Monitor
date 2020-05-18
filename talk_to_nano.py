dee0db55ed20a9ce940b7663d5cb843fbbb94edd
Ben <bhart@aut.ac.nz>
2020-05-18 23:43:42 +1200

    added data checks

--- talk_to_nano.py
+++ talk_to_nano.py
@@ -27,13 +27,31 @@ class Getnanoreadings:
           output = self.ser.readline()
           return output.decode().rstrip()
 
+     def data_clean(self, curr_data_point, past_data_point):
+          if not 0 <= curr_data_point <= 100:
+               x = past_data_point
+          else:
+               x = curr_data_point
+          low = past_data_point - past_data_point * 0.1
+          high = past_data_point + past_data_point * 0.1
+          if not low <= curr_data_point <= high:
+               x = past_data_point
+          else:
+               x = curr_data_point
+          return x
+
      def sql_insert(self):
           data = []
           data.append(time.ctime())
           print('reading data')
-          for i in range(8):
-               data.append(self.read_sensor(i+1))
           conn = sqlite3.connect('/home/pi/Documents/Garden-Monitor/data.db')
+          c = conn.cursor()
+          c.execute('SELECT * FROM from_nano ORDER BY ROWID DESC LIMIT 1')
+          last_data = c.fetchone()
+          for i in range(8):
+               curr_data = self.read_sensor(i+1)
+               data_point = self.data_clean(curr_data, last_data)
+               data.append(data_point)
           print('inserting data')
           c = conn.cursor()
           c.execute("INSERT INTO from_nano(datatime, humidity, air_temp1, methane_levels, CO_levels, pressure, air_temp2, soil_temp, soil_moisture) VALUES(?,?,?,?,?,?,?,?,?)", data)
